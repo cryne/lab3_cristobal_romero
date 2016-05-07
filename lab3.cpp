d16b7534466a44ffb921a036e5063f43ebd2b9dd
Cristobal Raul Romero Inestroza <crisonic123@hotmail.com>
2016-05-06 18:03:05 -0600

    revision de victoria diagonal

--- lab3.cpp
+++ lab3.cpp
@@ -32,19 +32,72 @@ int main(int argc, char const *argv[])
 				cout<<"jugada no valida"<<endl;
 			}
 		}
+		int jugada1;
 		for (int i = 5; i >=0 ; --i)
 		{
 			if(tablero[i][jugada]==0){
 				tablero[i][jugada]=1;
+				jugada1=i;
 				i=-1;
 			}
 		}
+		int victoria=0;
+		for (int i = 0; i < 4; ++i)
+		{
+			int x=jugada1-i,y=jugada+i;	
+			cout<<x<<"-f"<<y<<endl;	
+			if(x>-1&&x<6&&y>-1&&y<7){
+				if(tablero[x][y]==1){
+					++victoria;
+					cout<<"f"<<victoria<<endl;
+				}
+				if(tablero[x-1][y+1]==0){
+					i=5;
+					int x1=x,y1=y;
+					victoria=0;
+					for (int j = 0; j < 4; ++j)
+					{
+						int x1=x+j,y1=y-j;
+						cout<<x1<<"-g"<<y1<<endl;
+						if(x1>-1&&x1<6&&y1>-1&&y1<7){
+							if(tablero[x1][y1]==1){
+								++victoria;
+								cout<<"g"<<victoria<<endl;
+							}
+						}
+					}
+				}
+			}
+		}
+		/*if(victoria!=4){
+			victoria=0;
+			for (int i = 0; i < 4; ++i)
+			{
+				int x=jugada1+i,y=jugada;			
+				if(x>-1&&x<6&&y>-1&&y<7){
+					if(tablero[x][y]==1){
+						++victoria;
+					}
+				}
+			}
+		}*/
+
+		if(victoria==4){
+			estado=true;
+			cout<<"gano el jugador1"<<endl;
+		}		
+		for (int i = 0; i < 7; ++i)
+		{
+			cout<<i<<"\t";
+		}
+		cout<<endl;
+		cout<<"---------------------------------------------------"<<endl;
 		for (int i = 0; i < 6; ++i){
 			for (int j = 0; j < 7; ++j){
 				cout<<tablero[i][j]<<"\t";
 			}
 			cout<<endl;
-    	}
-    }
+		}
+	}
 	return 0;
 }
