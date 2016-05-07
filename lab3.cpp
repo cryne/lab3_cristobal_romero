a0f9b55b649f2bb470db8be95a9bae6fc07b4ede
Cristobal Raul Romero Inestroza <crisonic123@hotmail.com>
2016-05-06 18:32:58 -0600

    hacer jugador 2

--- lab3.cpp
+++ lab3.cpp
@@ -49,7 +49,7 @@ int main(int argc, char const *argv[])
 				if(tablero[x][y]==1){
 					++victoria;
 				}
-				if(tablero[x-1][y+1]==0){
+				if(tablero[x-1][y+1]==0||tablero[x-1][y+1]==2){
 					i=5;
 					int x1=x,y1=y;
 					victoria=0;
@@ -74,7 +74,7 @@ int main(int argc, char const *argv[])
 					if(tablero[x][y]==1){
 						++victoria;
 					}
-					if(tablero[x-1][y-1]==0){
+					if(tablero[x-1][y-1]==0||tablero[x-1][y-1]==2){
 						i=5;
 						int x1=x,y1=y;
 						victoria=0;
@@ -103,11 +103,32 @@ int main(int argc, char const *argv[])
 				}
 			}
 		}
-
-		if(victoria==4){
-			estado=true;
-			cout<<"gano el jugador1"<<endl;
-		}		
+		if(victoria!=4){
+			victoria=0;
+			for (int i = 0; i < 4; ++i)
+			{
+				int x=jugada1,y=jugada-i;	
+				if(x>-1&&x<6&&y>-1&&y<7){
+					if(tablero[x][y]==1){
+						++victoria;
+					}
+					if(tablero[x][y-1]==0||tablero[x][y-1]==2){
+						i=5;
+						int x1=x,y1=y;
+						victoria=0;
+						for (int j = 0; j < 4; ++j)
+						{
+							int x1=x,y1=y+j;
+							if(x1>-1&&x1<6&&y1>-1&&y1<7){
+								if(tablero[x1][y1]==1){
+									++victoria;
+								}
+							}
+						}
+					}
+				}
+			}
+		}				
 		for (int i = 0; i < 7; ++i)
 		{
 			cout<<i<<"\t";
@@ -119,6 +140,140 @@ int main(int argc, char const *argv[])
 				cout<<tablero[i][j]<<"\t";
 			}
 			cout<<endl;
+		}
+		if(victoria==4){
+			estado=true;
+			cout<<"gano el jugador1"<<endl;
+		}
+		if(!estado){
+
+
+
+
+
+
+			int jugada=-1;
+			while(jugada<0||jugada>6){
+				cout<<"jugador 2 ingrese la casilla en la que desea lanzar su pieza"<<endl;
+				cin>>jugada;
+				if (jugada<0||jugada>6)
+				{
+					cout<<"jugada no valida"<<endl;
+				}
+			}
+			int jugada1;
+			for (int i = 5; i >=0 ; --i)
+			{
+				if(tablero[i][jugada]==0){
+					tablero[i][jugada]=2;
+					jugada1=i;
+					i=-1;
+				}
+			}
+			int victoria=0;
+			for (int i = 0; i < 4; ++i)
+			{
+				int x=jugada1-i,y=jugada+i;	
+				if(x>-1&&x<6&&y>-1&&y<7){
+					if(tablero[x][y]==2){
+						++victoria;
+					}
+					if(tablero[x-1][y+1]==0||tablero[x-1][y+1]==1){
+						i=5;
+						int x1=x,y1=y;
+						victoria=0;
+						for (int j = 0; j < 4; ++j)
+						{
+							int x1=x+j,y1=y-j;
+							if(x1>-1&&x1<6&&y1>-1&&y1<7){
+								if(tablero[x1][y1]==2){
+									++victoria;
+								}
+							}
+						}
+					}
+				}
+			}
+			if(victoria!=4){
+				victoria=0;
+				for (int i = 0; i < 4; ++i)
+				{
+					int x=jugada1-i,y=jugada-i;	
+					if(x>-1&&x<6&&y>-1&&y<7){
+						if(tablero[x][y]==2){
+							++victoria;
+						}
+						if(tablero[x-1][y-1]==0||tablero[x-1][y-1]==1){
+							i=5;
+							int x1=x,y1=y;
+							victoria=0;
+							for (int j = 0; j < 4; ++j)
+							{
+								int x1=x+j,y1=y+j;
+								if(x1>-1&&x1<6&&y1>-1&&y1<7){
+									if(tablero[x1][y1]==2){
+										++victoria;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			if(victoria!=4){
+				victoria=0;
+				for (int i = 0; i < 4; ++i)
+				{
+					int x=jugada1+i,y=jugada;			
+					if(x>-1&&x<6&&y>-1&&y<7){
+						if(tablero[x][y]==2){
+							++victoria;
+						}
+					}
+				}
+			}
+			if(victoria!=4){
+				victoria=0;
+				for (int i = 0; i < 4; ++i)
+				{
+					int x=jugada1,y=jugada-i;	
+					if(x>-1&&x<6&&y>-1&&y<7){
+						if(tablero[x][y]==2){
+							++victoria;
+						}
+						if(tablero[x][y-1]==0||tablero[x][y-1]==1){
+							i=5;
+							int x1=x,y1=y;
+							victoria=0;
+							for (int j = 0; j < 4; ++j)
+							{
+								int x1=x,y1=y+j;
+								if(x1>-1&&x1<6&&y1>-1&&y1<7){
+									if(tablero[x1][y1]==2){
+										++victoria;
+									}
+								}
+							}
+						}
+					}
+				}
+			}				
+			for (int i = 0; i < 7; ++i)
+			{
+				cout<<i<<"\t";
+			}
+			cout<<endl;
+			cout<<"---------------------------------------------------"<<endl;
+			for (int i = 0; i < 6; ++i){
+				for (int j = 0; j < 7; ++j){
+					cout<<tablero[i][j]<<"\t";
+				}
+				cout<<endl;
+			}
+			if(victoria==4){
+				estado=true;
+				cout<<"gano el jugador2"<<endl;
+			}
 		}
 	}
 	return 0;
